b54df2bbab72823322b2480364de54a89454647a
Sapar Jumabekov <58820192+SapaTech@users.noreply.github.com>
2021-11-27 13:50:59 +0300

    Initial Commit

---
+++ SCInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SCInstagram
+//
+//  Created by user on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
